19f4ba6cd25f70fcddb6c5e68eab4f0c58b85d39
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-09 23:23:01 -0300

    feat: add error message (tooltip component)

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -1,7 +1,10 @@
+import { Tooltip } from '../Tooltip';
+
 type InputProps = {
   label: string;
   placeholder?: string;
   name?: string;
+  error?: string;
   type?: string;
   children?: JSX.Element;
 };
@@ -10,13 +13,25 @@ export function Input({
   name,
   type,
   placeholder,
+  error,
   children,
 }: InputProps) {
   return (
-    <div className="input-container">
-      <span className="input-container__label">{label}</span>
-      <input name={name} type={type} placeholder={placeholder} />
-      {children}
+    <div className="input">
+      <div className="input-container">
+        <span className="input-container__label">{label}</span>
+        <input
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          aria-invalid={Boolean(error) || undefined}
+          aria-describedby={
+            error ? `${name ? name + '-' : ''}error` : undefined
+          }
+        />
+        {children}
+      </div>
+      {!!error && <Tooltip name={name} text={error} />}
     </div>
   );
 }
