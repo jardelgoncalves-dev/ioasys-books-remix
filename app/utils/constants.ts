b18f68331bf32d1e5a65c894b8c0857ae5eed4b3
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-09 23:21:36 -0300

    feat: add constants

---
+++ app/utils/constants.ts
@@ -0,0 +1,4 @@
+export const COOKIE_SESSION_NAME = 'books__session';
+export const BASE_URL = 'https://books.ioasys.com.br/api/v1';
+export const TOKEN_KEY = '@ioasys-books/token';
+export const REFRESH_KEY = '@ioasys-books/refresh';
