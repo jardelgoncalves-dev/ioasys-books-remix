cbf9c4add095b226dab002de77bc55a24c7af741
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-13 19:55:01 -0300

    feat: add constatns to user logged

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,3 +2,4 @@ export const COOKIE_SESSION_NAME = 'books__session';
 export const BASE_URL = 'https://books.ioasys.com.br/api/v1';
 export const TOKEN_KEY = '@ioasys-books/token';
 export const REFRESH_KEY = '@ioasys-books/refresh';
+export const USER_LOGGED = '@ioasys-books/user';
